7c09631fbce6bfe9df524f6fd26956ebb3415f6e
Dung Vuong <dungcoder1213@gmail.com>
2023-03-13 14:17:46 -0700

    done

--- pizza/forms.py
+++ pizza/forms.py
@@ -8,7 +8,6 @@ from .models import Pizza, Size
 
 class PizzaForm(forms.ModelForm):
 
-    size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
 
     class Meta:
         model = Pizza
